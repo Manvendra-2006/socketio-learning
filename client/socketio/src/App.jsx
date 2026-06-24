189bbdfbb000577f0e05e89c985e398c5621c5c5
Manvendra_2006 <manvendrabhardwaj8@gmail.com>
2026-06-24 12:08:09 +0530

    feat: integrate Socket.IO with JWT auth, cookies, and room-based messaging

--- client/socketio/src/App.jsx
+++ client/socketio/src/App.jsx
@@ -1,20 +1,59 @@
 import React from 'react'
+import { useMemo } from 'react'
+import { useState } from 'react'
 import { useEffect } from 'react'
-import {io} from "socket.io-client"
+import { io } from "socket.io-client"
 const App = () => {
-  const socket = io("http://localhost:3000/")
-
-  useEffect(()=>{
-    socket.on("connect",()=>{
-      console.log("coonected",socket.id)
+  const socket = useMemo(() => io("http://localhost:3000/"), []) // Here Usememo prevent from creating multiple socket on render 
+  const [message, setmessage] = useState('')
+  const [room,setroom]= useState("")
+  const [hello,sethello] = useState("")
+   const [socketId,setsocketId] = useState("")
+   const  [roomName,setroomName] = useState("")
+  function handleSubmit(e) {
+    e.preventDefault();
+    socket.emit("message", {message,room})
+  }
+  function handleRoom(e){
+    e.preventDefault()
+    socket.emit("join-room",roomName)
+    setroomName(" ")
+  }
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("connected", socket.id)
+      setsocketId(socket.id)
     })
-    socket.on("Welcome",(s)=>{
+    socket.on("Welcome", (s) => {
       console.log(s)
     })
-  },[])
+  //    if (room) {
+  //   socket.emit("join-room", room);
+  // }
+    socket.on("message", (s) => {
+      console.log(s)
+      sethello(s)
+    })
+    return () => {
+      console.log("CleanUp")
+      socket.disconnect()
+    }
+  }, [])
   return (
     <div>
-      
+      <h1>Welcome to the Socket Io</h1>
+      <h1>{socketId}</h1>
+      <form action="" onSubmit={handleRoom}>
+        <label>RoomName</label>
+        <input type="text" placeholder="enter text" value={roomName} onChange={(event)=>setroomName(event.target.value)}/>
+        <button>Join</button>
+      </form>
+      <form action="" onSubmit={handleSubmit}>
+        <input type="text" value={message} onChange={(e) => setmessage(e.target.value)} placeholder="Enter Text" />
+        <input type="text"  value={room} onChange={(e)=>setroom(e.target.value)}/>
+        <h1>{hello}</h1>
+        <button>Send</button>
+      </form>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ import express from 'express'
 import { Server } from 'socket.io';
 import {createServer} from "http"
 import cors from 'cors'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser';
 const app = express();
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:"http://localhost:5173",
+    credentials:true
 }))
+app.use(cookieParser())
 const server  = createServer(app)
 const io = new Server(server,{
     cors: {
@@ -14,11 +18,38 @@ const io = new Server(server,{
     credentials:true
   }
 })
+io.use((socket,next)=>{ // middleware
+    console.log(socket)
+    next() // after this io.on code will run otherwise not 
+})
 io.on("connection",(socket)=>{
     console.log("User is Connected")
     console.log("ID",socket.id)
-    socket.emit("Welcome","Welcome to the server")
-    socket.broadcast.emit("Welcome","Welcome to the server");
+    socket.broadcast.emit("Welcome",`User conneted ${socket.id}`)    
+    socket.on("message",(data)=>{
+        console.log(data);
+        // socket.broadcast.emit("message",data) // This will send data to other except itself
+        io.to(data.room).emit("message",data.message)
+    })
+    socket.on("disconnect",(s)=>{
+        console.log(s)
+    })
+    socket.on("join-room",(room)=>{
+        socket.join(room);
+        console.log(room)
+    })
+})
+app.get("/",(req,resp)=>{
+    resp.send("Hello World")
+})
+app.get("/login",(req,resp)=>{
+    const token = jwt.sign(
+        {_id:"oidndkokdowmdw"},
+       " 12222"
+    )
+    resp.cookie("token",token,{httpOnly:true,secure:true,sameSite:"none"}).json({
+        message:"Login Successfully"
+    })
 })
 server.listen(3000,()=>{ // Here we write server instead of app because Socket.io server is built on HTTP server and if we write app.listen then it internally create one more HTTP server which is connected but we want a http server to connect which is attach to Socket..io 
     console.log("Server is running on Port 3000");
